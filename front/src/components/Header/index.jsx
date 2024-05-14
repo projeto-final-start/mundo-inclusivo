09825b888b3de96bd15a547c7213844d94e5250d
Willian Gabriel <willianlopes20@hotmail.com>
2024-05-14 13:35:49 -0300

    first commit

---
+++ front/src/components/Header/index.jsx
@@ -0,0 +1,20 @@
+import styles from './Header.module.css'
+
+function Header () {
+    return (
+        <header className={styles.header}>
+            {/* <span>Sintonia Mental</span> */}
+            <span><img src="./src/assets/logo.svg" alt="Logo" /></span>
+            <nav>
+                <a href="">Home</a>
+                <a href="">Contato</a>
+                <a href="">Tratamento</a>                
+            </nav>
+                <button>Login/Cadastro</button>
+                
+
+        </header>
+    )
+}
+
+export default Header
