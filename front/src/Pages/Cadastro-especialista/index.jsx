9ee8f592b8435fbe824e3538d32b7b4a709950a2
Layla Lima <laylagomess@outlook.com>
2024-05-16 19:26:01 -0300

    Alteração da pagina Cadastro, e aleração na pagina Login.

--- front/src/Pages/Cadastro-especialista/index.jsx
+++ front/src/Pages/Cadastro-especialista/index.jsx
@@ -9,25 +9,36 @@ function CadastroEspecialista() {
         <>
             <Header />
             <Container>
-                <div className="Cadastro-page">
+                <div className="cadastro-page">
                     <figure>
                         <img src="front\public\backgroundlogin.svg" alt="imagem de fundo" />
                     </figure>
-
-                    <div>
-                        <form action="Cadastro">
-                            <h1 className="Cadastro">Cadastro</h1>
-                            <input className="Nome-cad" type="text" id="nome" placeholder=" Nome e Sobrenome" />
-
+                   
+                    <div className="box-cad1">
+                    <h1 className="cadastro">Cadastro</h1>
+                        <form action="formulario1">
+                            <input className="nome-cad" type="text" id="nome" placeholder=" Nome e Sobrenome" />
                             <input className="email-cad" type="email" id="email" placeholder="E-mail" />
-
-                            <input className="Senha-cad" type="password" id="senha" placeholder="Senha" />
+                            <input className="senha-cad" type="password" id="senha" placeholder="Senha" />
                             <input className="numero-cad" type="text" id="numero" placeholder="Número de celular" />
-                            <input className="CPF-cad" type="text" id="CPF" placeholder="CPF" />
-                            <input className="genero-cad" type="list" datalist="Masculino" id="genero" placeholder="Gênero" />
+                            <input className="cpf-cad" type="text" id="CPF" placeholder="CPF" />
+                            <select className="genero-cad" type="text" id="genero">
+                                <option value="genero">Gênero</option>
+                                <option value="feminino">Feminino</option>
+                                <option value="masculino">Masculino</option>
+                                <option value="outro">Outro</option>
+                            </select>
                         </form>
-                    </div>
-
+                        </div >
+                        <div className='box-cad1'>
+                        <form action="formulario2">
+                            <button type="image" src="front\src\assets\comunicacao.svg" alt=""></button>
+                            <button type="image" src="front\src\assets\comunicacao.svg" alt=""></button>
+                            <button type="image" src="front\src\assets\comunicacao.svg" alt=""></button>
+                            <input className="crp-cad" type="number" id="crp" placeholder="CRP" />
+                            <input className="ano-cad" type="date" id="ano" placeholder="Em que ano você começou a realizar consulta" />
+                        </form>    
+                        </div>   
                 </div>
             </Container>
             <Footer />
