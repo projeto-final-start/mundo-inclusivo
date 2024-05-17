c4fc6e586e23d9cb83aa531a74feb99960a7edbe
HEWESON <hewesonn@icloud.com>
2024-05-16 21:28:13 -0300

    Reorganizacao das rotas

--- front/src/Pages/Login/index.jsx
+++ front/src/Pages/Login/index.jsx
@@ -1,49 +1,64 @@
 import "./Login.css";
-import Header from "../../components/Header";
-import Container from '../../components/Container';
-import Footer from "../../components/Footer";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Login() {
   return (
     <>
-      <Header />
-      <Container>
-          <figure>
-            <img src="./src/assets/background.svg" alt="background-img" className="background-image" />
-          </figure>
-          <div className="login-page">
-          <div className="caixa-login">
-            <form className="form-caixa">
+      <figure>
+        <img
+          src="./src/assets/background.svg"
+          alt="background-img"
+          className="background-image"
+        />
+      </figure>
+      <div className="login-page">
+        <div className="caixa-login">
+          <form className="form-caixa">
             <h1 className="login">Login</h1>
-              <input className="email" type="email" id="email-login" placeholder="E-mail ou CPF" />
-              <input className="senha" type="password" id="senha-login" placeholder="Senha" />
+            <input
+              className="email"
+              type="email"
+              id="email-login"
+              placeholder="E-mail ou CPF"
+            />
+            <input
+              className="senha"
+              type="password"
+              id="senha-login"
+              placeholder="Senha"
+            />
 
+            <div className="caixa">
+              <input type="checkbox" id="remember" name="lembre-me" />
+              <label className="remember-login" htmlFor="lembre-me">
+                Lembre-me
+              </label>
+              <Link className="semsenha" to="#">
+                Esqueceu a senha?
+              </Link>
+            </div>
 
-              <div className="caixa">
-                <input type="checkbox" id="remember" name="lembre-me" />
-                <label className="remember-login" htmlFor="lembre-me">Lembre-me</label>
-                <Link className="semsenha" to="#">Esqueceu a senha?</Link>
-              </div>
+            <button className="botao-login" type="submit">
+              Login
+            </button>
 
-              <button className="botao-login" type="submit">Login</button>
+            <div className="semconta-grupo">
+              <label className="semconta" htmlFor="Não tem conta?">
+                Não tem conta?
+              </label>
+              <Link className="criarconta" to="#">
+                Criar nova conta
+              </Link>
+            </div>
+          </form>
 
-              <div className="semconta-grupo">
-                <label className="semconta" htmlFor="Não tem conta?">Não tem conta?</label>
-                <Link className="criarconta" to="#">Criar nova conta</Link>
-              </div>
-
-            </form >
-
-            <figure>
-              <img src="front\public\fotologin.svg" alt="Foto" className="foto" />
-            </figure>
-          </div>
+          <figure>
+            <img src="front\public\fotologin.svg" alt="Foto" className="foto" />
+          </figure>
         </div>
-      </Container>
-      <Footer />
+      </div>
     </>
-  )
+  );
 }
 
-export default Login
+export default Login;
--- front/src/Pages/Page404/index.jsx
+++ front/src/Pages/Page404/index.jsx
@@ -1,26 +1,15 @@
-import Header from '../../components/Header'
-import Container from '../../components/Container'
-import Footer from '../../components/Footer'
-import './Page404.css'
+import "./Page404.css";
 
-
-function Page404 () {
-    return (
-        <>
-            <Header/>
-            <Container>
-                <h2 className='titulo2-404'>Algo de errado não está certo!</h2>
-                <div className='texto-404'>
-                    <span className='texto_grande-404'>404</span> <br />
-                    <strong className='texto_preto-404'>Página Não Localizada</strong>
-                </div>
-            </Container>
-            <Footer/>
-        
-        </>
-    )
+function Page404() {
+  return (
+    <>
+      <h2 className="titulo2-404">Algo de errado não está certo!</h2>
+      <div className="texto-404">
+        <span className="texto_grande-404">404</span> <br />
+        <strong className="texto_preto-404">Página Não Localizada</strong>
+      </div>
+    </>
+  );
 }
 
-export default Page404
-
-
+export default Page404;
