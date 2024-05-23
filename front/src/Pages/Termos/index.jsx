d701d501c24d30604dc930cfd2c88c00e5a74c37
HEWESON <hewesonn@icloud.com>
2024-05-23 08:20:21 -0300

    Finalizacao do CSS das paginas Termos de Uso e Politica e Privacidade

--- front/src/Pages/Termos/index.jsx
+++ front/src/Pages/Termos/index.jsx
@@ -1,129 +1,181 @@
+import "./Termos.css";
 function Termos() {
-    return(
-        <section>
-        <div>
+  return (
+    <section>
+      <div className="title-termos-usuario">
         <h2>TERMOS E CONDIÇÕES GERAIS DE USO - USUÁRIO</h2>
+      </div>
+      <div>
         <br />
-        <p>Bem-vindo aos Termos e Condições Gerais de Uso do site de Saúde Mental para pais de autistas. 
-        Ao acessar ou usar este site, você concorda com estes termos e condições. Se você não concordar com estes termos,
-        por favor, não use este site.</p>
-         <br />
-        <h3>1. Descrição do Site:</h3>
-        <br />
-        <p>Este site foi desenvolvido para fornecer recursos, informações e suporte relacionados à saúde mental para pais de 
-        crianças autistas. Ele inclui conteúdos educacionais, artigos, fóruns de discussão e acesso a profissionais especializados.</p>
-        <br />
-        <h3>2. Uso do Site:</h3>
-        <br />
-        <p>Ao usar este site, você concorda em não violar nenhuma lei aplicável. Você concorda em não usar este site de qualquer 
-        forma que possa danificar, desativar, sobrecarregar ou prejudicar o site ou interferir no uso e aproveitamento de outros
-        usuários.</p>
-        <br />
-        <h3>3. Informações e Conteúdo:</h3>
-        <br />
-        <p>As informações fornecidas neste site são apenas para fins informativos e não substituem o aconselhamento médico ou
-        psicológico profissional. Não nos responsabilizamos por quaisquer decisões tomadas com base nas informações 
-        fornecidas neste site.</p>
-        <br />
-        <h3>4. Privacidade:</h3>
-        <br />
-        <p>Nossa Política de Privacidade descreve como coletamos, usamos e protegemos suas informações pessoais. Ao usar este 
-        site, você concorda com as práticas descritas em nossa Política de Privacidade.</p>
-        <br />
-        <h3>5. Fóruns e Comentários:</h3>
-        <br />
-        <p>Se este site incluir fóruns de discussão ou áreas para comentários, você concorda em usar esses recursos de forma 
-        responsável. Não toleramos linguagem abusiva, difamatória, obscena ou ameaçadora.</p>
-        <br />
-        <h3>6. Propriedade Intelectual:</h3>
-        <br />
-        <p>Todo o conteúdo deste site, incluindo textos, gráficos, logotipos, ícones e imagens, é propriedade do site ou de seus 
-        licenciadores e está protegido por leis de direitos autorais e outras leis de propriedade intelectual.</p>
-        <br />
-        <h3>7. Links para Terceiros:</h3>
-        <br />
-        <p>Este site pode conter links para sites de terceiros. Não controlamos ou endossamos o conteúdo desses sites e não nos 
-        responsabilizamos por qualquer dano ou prejuízo decorrente do uso desses sites.</p>
-        <br />
-        <h3>8. Alterações nos Termos e Condições:</h3>
-        <br />
-        <p>Reservamo-nos o direito de modificar estes Termos e Condições a qualquer momento, sem aviso prévio. O uso 
-        continuado deste site após tais modificações constitui sua aceitação dos Termos e Condições modificados.</p>
-        <br />
-        <h3>9. Rescisão:</h3>
-        <br />
-        <p>Reservamo-nos o direito de encerrar ou restringir seu acesso a este site, sem aviso prévio, por qualquer motivo, a nosso 
-        exclusivo critério.Reservamo-nos o direito de encerrar ou restringir seu acesso a este site, sem aviso prévio, por qualquer motivo, a nosso 
-        exclusivo critério.</p>
-        <br />
-        <h3>10. Lei Aplicável:</h3>
-        <br />
-        <p>Estes Termos e Condições serão regidos e interpretados de acordo com as leis vigentes. Qualquer disputa relacionada 
-        a estes Termos e Condições será submetida à jurisdição exclusiva dos tribunais competentes.</p>
-        <br />
-        <p>Se você tiver alguma dúvida ou preocupação sobre estes Termos e Condições, entre em contato conosco.</p>
-        <br />
-        <h2>TERMOS E CONDIÇÕES GERAIS DE USO - ESPECIALISTA</h2>
-        <br />
-        <p>Bem-vindo aos Termos e Condições Gerais de Uso do site de saúde mental para pais de autistas, na qualidade de 
-        Especialista. Ao acessar ou usar este site, você concorda com estes termos e condições. Se você não concordar com 
-        estes termos, por favor, não use este site.</p>
-        <br />
-        <h3>1. Descrição do Site:</h3>
-        <br />
-        <p>Este site foi desenvolvido para fornecer recursos, informações e suporte relacionados à saúde mental para pais de 
-        crianças autistas. Ele inclui conteúdos educacionais, artigos, fóruns de discussão e acesso a profissionais especializados.</p>
-        <br />
-        <h3>2. Responsabilidades do Especialista:</h3>
-        <br />
-        <p>Ao usar este site na qualidade de Especialista, você concorda em fornecer informações precisas, atualizadas e relevantes.
-        Você entende que as informações fornecidas são apenas para fins informativos e não substituem o aconselhamento 
-        médico ou psicológico profissional.</p>
-        <br />
-        <h3>3. Confidencialidade:</h3>
-        <br />
-        <p>Você concorda em manter a confidencialidade de qualquer informação confidencial compartilhada pelos usuários do site. 
-        Você entende que é proibido divulgar informações pessoais ou identificáveis sobre os usuários sem seu consentimento.</p>
-        <br />
-        <h3>4. Interação com Usuários:</h3>
-        <br />
-        <p>Ao interagir com os usuários deste site, você concorda em fazê-lo de maneira respeitosa, ética e profissional. Não 
-        toleramos linguagem abusiva, difamatória, obscena ou ameaçadora.</p>
-        <br />
-        <h3>5. Propriedade Intelectual:</h3>
-        <br />
-        <p>Todo o conteúdo deste site, incluindo textos, gráficos, logotipos, ícones e imagens, é propriedade do site ou de seus 
-        licenciadores e está protegido por leis de direitos autorais e outras leis de propriedade intelectual.</p>
-        <br />
-        <h3>6. Links para Terceiros:</h3>
-        <br />
-        <p>Este site pode conter links para sites de terceiros. Não controlamos ou endossamos o conteúdo desses sites e não nos 
-        responsabilizamos por qualquer dano ou prejuízo decorrente do uso desses sites.</p>
-        <br />
-        <h3>7. Alterações nos Termos e Condições:</h3>
-        <br />
-        <p>Reservamo-nos o direito de modificar estes Termos e Condições a qualquer momento, sem aviso prévio. O uso 
-        continuado deste site após tais modificações constitui sua aceitação dos Termos e Condições modificados.</p>
-        <br />
-        <h3>8. Rescisão:</h3>
-        <br />
-        <p>Reservamo-nos o direito de encerrar ou restringir seu acesso a este site, sem aviso prévio, por qualquer motivo, a nosso 
-        exclusivo critério.</p>
-        <br />
-        <h3>9. Lei Aplicável:</h3>
-        <br />
-        <p>Estes Termos e Condições serão regidos e interpretados de acordo com as leis vigentes. Qualquer disputa relacionada 
-        a estes Termos e Condições será submetida à jurisdição exclusiva dos tribunais competentes.</p>
-        <br />
-        <p>Se você tiver alguma dúvida ou preocupação sobre estes Termos e Condições, entre em contato conosco.</p>
-        <br />
+        <div>
+          <p className="subtitle-termos-usuario">
+            Bem-vindo aos Termos e Condições Gerais de Uso do site de Saúde
+            Mental para pais de autistas. Ao acessar ou usar este site, você
+            concorda com estes termos e condições. Se você não concordar com
+            estes termos, por favor, não use este site.
+          </p>
+        </div>
+        <h3 className="topicos-termos">1. Descrição do Site:</h3>
+        <p className="paragrafos-termos">
+          Este site foi desenvolvido para fornecer recursos, informações e
+          suporte relacionados à saúde mental para pais de crianças autistas.
+          Ele inclui conteúdos educacionais, artigos, fóruns de discussão e
+          acesso a profissionais especializados.
+        </p>
+        <h3 className="topicos-termos">2. Uso do Site:</h3>
+        <p className="paragrafos-termos">
+          Ao usar este site, você concorda em não violar nenhuma lei aplicável.
+          Você concorda em não usar este site de qualquer forma que possa
+          danificar, desativar, sobrecarregar ou prejudicar o site ou interferir
+          no uso e aproveitamento de outros usuários.
+        </p>
+        <h3 className="topicos-termos">3. Informações e Conteúdo:</h3>
+        <p className="paragrafos-termos">
+          As informações fornecidas neste site são apenas para fins informativos
+          e não substituem o aconselhamento médico ou psicológico profissional.
+          Não nos responsabilizamos por quaisquer decisões tomadas com base nas
+          informações fornecidas neste site.
+        </p>
+        <h3 className="topicos-termos">4. Privacidade:</h3>
+        <p className="paragrafos-termos">
+          Nossa Política de Privacidade descreve como coletamos, usamos e
+          protegemos suas informações pessoais. Ao usar este site, você concorda
+          com as práticas descritas em nossa Política de Privacidade.
+        </p>
+        <h3 className="topicos-termos">5. Fóruns e Comentários:</h3>
+        <p className="paragrafos-termos">
+          Se este site incluir fóruns de discussão ou áreas para comentários,
+          você concorda em usar esses recursos de forma responsável. Não
+          toleramos linguagem abusiva, difamatória, obscena ou ameaçadora.
+        </p>
+        <h3 className="topicos-termos">6. Propriedade Intelectual:</h3>
+        <p className="paragrafos-termos">
+          Todo o conteúdo deste site, incluindo textos, gráficos, logotipos,
+          ícones e imagens, é propriedade do site ou de seus licenciadores e
+          está protegido por leis de direitos autorais e outras leis de
+          propriedade intelectual.
+        </p>
+        <h3 className="topicos-termos">7. Links para Terceiros:</h3>
+        <p className="paragrafos-termos">
+          Este site pode conter links para sites de terceiros. Não controlamos
+          ou endossamos o conteúdo desses sites e não nos responsabilizamos por
+          qualquer dano ou prejuízo decorrente do uso desses sites.
+        </p>
+        <h3 className="topicos-termos">
+          8. Alterações nos Termos e Condições:
+        </h3>
 
-         </div>
-         <figure>
-         <img className="/src/assets/img-termos" src="/img-termos.svg" alt="Imagem Termos De Uso" />
-       </figure>
-       </section>
-    )
+        <p className="paragrafos-termos">
+          Reservamo-nos o direito de modificar estes Termos e Condições a
+          qualquer momento, sem aviso prévio. O uso continuado deste site após
+          tais modificações constitui sua aceitação dos Termos e Condições
+          modificados.
+        </p>
+        <h3 className="topicos-termos">9. Rescisão:</h3>
+        <p className="paragrafos-termos">
+          Reservamo-nos o direito de encerrar ou restringir seu acesso a este
+          site, sem aviso prévio, por qualquer motivo, a nosso exclusivo
+          critério.Reservamo-nos o direito de encerrar ou restringir seu acesso
+          a este site, sem aviso prévio, por qualquer motivo, a nosso exclusivo
+          critério.
+        </p>
+        <h3 className="topicos-termos">10. Lei Aplicável:</h3>
+        <p className="paragrafos-termos">
+          Estes Termos e Condições serão regidos e interpretados de acordo com
+          as leis vigentes. Qualquer disputa relacionada a estes Termos e
+          Condições será submetida à jurisdição exclusiva dos tribunais
+          competentes.
+        </p>
+        <p className="paragrafos-termos">
+          Se você tiver alguma dúvida ou preocupação sobre estes Termos e
+          Condições, entre em contato conosco.
+        </p>
+        <hr className="line"/>
+        <h2 className="title-termos-especialista">
+          TERMOS E CONDIÇÕES GERAIS DE USO - ESPECIALISTA
+        </h2>
+        <p className="subtitle-termos-especialista">
+          Bem-vindo aos Termos e Condições Gerais de Uso do site de saúde mental
+          para pais de autistas, na qualidade de Especialista. Ao acessar ou
+          usar este site, você concorda com estes termos e condições. Se você
+          não concordar com estes termos, por favor, não use este site.
+        </p>
+        <h3 className="topicos-termos">1. Descrição do Site:</h3>
+        <p className="paragrafos-termos">
+          Este site foi desenvolvido para fornecer recursos, informações e
+          suporte relacionados à saúde mental para pais de crianças autistas.
+          Ele inclui conteúdos educacionais, artigos, fóruns de discussão e
+          acesso a profissionais especializados.
+        </p>
+        <h3 className="topicos-termos">
+          2. Responsabilidades do Especialista:
+        </h3>
+        <p className="paragrafos-termos">
+          Ao usar este site na qualidade de Especialista, você concorda em
+          fornecer informações precisas, atualizadas e relevantes. Você entende
+          que as informações fornecidas são apenas para fins informativos e não
+          substituem o aconselhamento médico ou psicológico profissional.
+        </p>
+        <h3 className="topicos-termos">3. Confidencialidade:</h3>
+        <p className="paragrafos-termos">
+          Você concorda em manter a confidencialidade de qualquer informação
+          confidencial compartilhada pelos usuários do site. Você entende que é
+          proibido divulgar informações pessoais ou identificáveis sobre os
+          usuários sem seu consentimento.
+        </p>
+        <h3 className="topicos-termos">4. Interação com Usuários:</h3>
+        <p className="paragrafos-termos">
+          Ao interagir com os usuários deste site, você concorda em fazê-lo de
+          maneira respeitosa, ética e profissional. Não toleramos linguagem
+          abusiva, difamatória, obscena ou ameaçadora.
+        </p>
+        <h3 className="topicos-termos">5. Propriedade Intelectual:</h3>
+        <p className="paragrafos-termos">
+          Todo o conteúdo deste site, incluindo textos, gráficos, logotipos,
+          ícones e imagens, é propriedade do site ou de seus licenciadores e
+          está protegido por leis de direitos autorais e outras leis de
+          propriedade intelectual.
+        </p>
+        <h3 className="topicos-termos">6. Links para Terceiros:</h3>
+        <p className="paragrafos-termos">
+          Este site pode conter links para sites de terceiros. Não controlamos
+          ou endossamos o conteúdo desses sites e não nos responsabilizamos por
+          qualquer dano ou prejuízo decorrente do uso desses sites.
+        </p>
+        <h3 className="topicos-termos">
+          7. Alterações nos Termos e Condições:
+        </h3>
+        <p className="paragrafos-termos">
+          Reservamo-nos o direito de modificar estes Termos e Condições a
+          qualquer momento, sem aviso prévio. O uso continuado deste site após
+          tais modificações constitui sua aceitação dos Termos e Condições
+          modificados.
+        </p>
+        <h3 className="topicos-termos">8. Rescisão:</h3>
+        <p className="paragrafos-termos">
+          Reservamo-nos o direito de encerrar ou restringir seu acesso a este
+          site, sem aviso prévio, por qualquer motivo, a nosso exclusivo
+          critério.
+        </p>
+        <h3 className="topicos-termos">9. Lei Aplicável:</h3>
+        <p className="paragrafos-termos">
+          Estes Termos e Condições serão regidos e interpretados de acordo com
+          as leis vigentes. Qualquer disputa relacionada a estes Termos e
+          Condições será submetida à jurisdição exclusiva dos tribunais
+          competentes.
+        </p>
+        <p className="paragrafos-termos">
+          Se você tiver alguma dúvida ou preocupação sobre estes Termos e
+          Condições, entre em contato conosco.
+        </p>
+      </div>
+      <img
+        className="img-termos"
+        src="/src/assets/img-termos.svg"
+        alt="Imagem Termos De Uso"
+      />
+    </section>
+  );
 }
 
 export default Termos;
